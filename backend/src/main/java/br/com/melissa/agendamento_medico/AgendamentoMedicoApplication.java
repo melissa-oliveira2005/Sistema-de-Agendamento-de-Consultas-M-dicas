7ef232cbfdc57dbbac1aedf2c6619184a8419506
melissa-oliveira2005 <mcao2@aluno.ifal.edu.br>
2026-06-15 10:34:42 -0300

    Projeto Spring Boot inicial

---
+++ backend/src/main/java/br/com/melissa/agendamento_medico/AgendamentoMedicoApplication.java
@@ -0,0 +1,13 @@
+package br.com.melissa.agendamento_medico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentoMedicoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgendamentoMedicoApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/br/com/melissa/agendamento_medico/AgendamentoMedicoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.melissa.agendamento_medico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentoMedicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
