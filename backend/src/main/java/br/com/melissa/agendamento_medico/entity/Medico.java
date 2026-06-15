b438b889c88d410a14b4db897e52aba93120c03b
melissa-oliveira2005 <mcao2@aluno.ifal.edu.br>
2026-06-15 12:01:32 -0300

    Refatoração das entidades ilizando Lombok

--- backend/src/main/java/br/com/melissa/agendamento_medico/entity/Medico.java
+++ backend/src/main/java/br/com/melissa/agendamento_medico/entity/Medico.java
@@ -1,52 +1,18 @@
 package br.com.melissa.agendamento_medico.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Medico {
 
     private Long id;
     private String nome;
     private String crm;
     private String especialidade;
+    private String email;
 
-    public Medico() {
-    }
-
-    public Medico(Long id, String nome,
-                  String crm, String especialidade) {
-        this.id = id;
-        this.nome = nome;
-        this.crm = crm;
-        this.especialidade = especialidade;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCrm() {
-        return crm;
-    }
-
-    public void setCrm(String crm) {
-        this.crm = crm;
-    }
-
-    public String getEspecialidade() {
-        return especialidade;
-    }
-
-    public void setEspecialidade(String especialidade) {
-        this.especialidade = especialidade;
-    }
 }
--- backend/src/main/java/br/com/melissa/agendamento_medico/entity/Paciente.java
+++ backend/src/main/java/br/com/melissa/agendamento_medico/entity/Paciente.java
@@ -1,51 +1,18 @@
 package br.com.melissa.agendamento_medico.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Paciente {
+
     private Long id;
     private String nome;
     private String cpf;
     private String telefone;
     private String email;
 
-    public Paciente() {
-    }
-
-    public Paciente(Long id, String nome, String cpf, String telefone, String email) {
-        this.id = id;
-        this.nome = nome;
-        this.cpf = cpf;
-        this.telefone = telefone;
-        this.email = email;
-    }
-
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getNome() {
-        return nome;
-    }
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-    public String getCpf() {
-        return cpf;
-    }
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-    public String getTelefone() {
-        return telefone;
-    }
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
